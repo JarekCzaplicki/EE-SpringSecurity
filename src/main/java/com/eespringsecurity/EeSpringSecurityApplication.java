fe3db8a1b5914c84a4a9be96e84d6d436be4574c
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-12-14 08:42:01 +0100

    Initial commit

---
+++ src/main/java/com/eespringsecurity/EeSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.eespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eespringsecurity/EeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
