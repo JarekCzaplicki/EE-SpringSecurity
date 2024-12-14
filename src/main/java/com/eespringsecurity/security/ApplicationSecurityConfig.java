6a94e9f5ac73c34898e6f18d32d3d85ecf859375
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-12-14 10:27:38 +0100

    Spring boot security v 6

--- src/main/java/com/eespringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/eespringsecurity/security/ApplicationSecurityConfig.java
@@ -1,19 +1,24 @@
 package com.eespringsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "index.html").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+                .authorizeHttpRequests(a -> a
+                .requestMatchers("/", "index.html").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
