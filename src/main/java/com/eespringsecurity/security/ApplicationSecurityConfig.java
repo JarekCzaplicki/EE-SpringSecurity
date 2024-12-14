9a3e65eb67119b0d7f3739c7e4f4e76cb097f58d
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-12-14 11:17:08 +0100

    Spring boot security v 6

--- src/main/java/com/eespringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/eespringsecurity/security/ApplicationSecurityConfig.java
@@ -1,17 +1,47 @@
 package com.eespringsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails student = User.builder()
+                .username("student")
+                .password(passwordEncoder.encode("123456"))
+                .roles("Student")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("123456"))
+                .roles("admin")
+                .build();
+        return new InMemoryUserDetailsManager(student, admin);
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/", "index.html").permitAll()
+                .antMatchers("/api/**").hasRole("student")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
